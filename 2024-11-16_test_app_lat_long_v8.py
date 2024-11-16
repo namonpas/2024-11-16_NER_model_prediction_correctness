12fef20c2fef2b050b63d35038e9bfa0bea80468
namonpas <62482552+namonpas@users.noreply.github.com>
2024-11-16 23:59:01 +0700

    Add files via upload

--- 2024-11-16_test_app_lat_long_v8.py
+++ 2024-11-16_test_app_lat_long_v8.py
@@ -16,6 +16,7 @@ import matplotlib.font_manager as fm
 from PIL import ImageFont, ImageDraw, Image
 import numpy as np
 from wordcloud import WordCloud, get_single_color_func
+import os
 
 # theme
 primary_color = "#1e90ff"
@@ -735,9 +736,24 @@ if __name__ == "__main__":
 
 # Load the pre-trained model
 @st.cache_data
+# def load_model():
+#     model = joblib.load("NER_model.joblib")
+#     return model
+
+# model = load_model()
+
 def load_model():
-    model = joblib.load("NER_model.joblib")
-    return model
+    model_path = 'NER_model.joblib'
+    if os.path.exists(model_path):
+        try:
+            model = joblib.load(model_path)
+            return model
+        except Exception as e:
+            st.error(f"Error loading model: {str(e)}")
+            return None
+    else:
+        st.error(f"Model file {model_path} not found.")
+        return None
 
 model = load_model()
 
